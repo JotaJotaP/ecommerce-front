64c762b8002c7cd6a4095ba6e574ae6daeef2f9d
JotaJotaP <joaojose.cmpinto@gmail.com>
2025-02-25 16:31:49 +0000

    Updates

---
+++ precache-manifest.d7c9ab3187d64604f78e621463dbcbff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72d004d6e04d35ff5ba421d0fe4326df",
+    "url": "/e-commerce-front/index.html"
+  },
+  {
+    "revision": "f13e7d43fb7dc91f1dbb",
+    "url": "/e-commerce-front/static/css/2.5ab72513.chunk.css"
+  },
+  {
+    "revision": "02f8cd2b9caaf137343d",
+    "url": "/e-commerce-front/static/css/main.6d12e6c6.chunk.css"
+  },
+  {
+    "revision": "f13e7d43fb7dc91f1dbb",
+    "url": "/e-commerce-front/static/js/2.f2e30447.chunk.js"
+  },
+  {
+    "revision": "02f8cd2b9caaf137343d",
+    "url": "/e-commerce-front/static/js/main.0924aa80.chunk.js"
+  },
+  {
+    "revision": "35f8e8ebe64255e8fb8d",
+    "url": "/e-commerce-front/static/js/runtime~main.f03e0a80.js"
+  }
+]);
