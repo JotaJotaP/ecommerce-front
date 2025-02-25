c1718e2ac81866b36f3c81fa86b00da5f7eb0035
JotaJotaP <joaojose.cmpinto@gmail.com>
2025-02-25 01:34:26 +0000

    Deploy build to GitHub Pages

---
+++ build/precache-manifest.afbbb0c1da75fb1e0a2091b98c2bce8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f17db85e2dcb17e65ef5eb1bcae0d282",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f13e7d43fb7dc91f1dbb",
+    "url": "/static/css/2.5ab72513.chunk.css"
+  },
+  {
+    "revision": "02f8cd2b9caaf137343d",
+    "url": "/static/css/main.6d12e6c6.chunk.css"
+  },
+  {
+    "revision": "f13e7d43fb7dc91f1dbb",
+    "url": "/static/js/2.f2e30447.chunk.js"
+  },
+  {
+    "revision": "02f8cd2b9caaf137343d",
+    "url": "/static/js/main.0924aa80.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
