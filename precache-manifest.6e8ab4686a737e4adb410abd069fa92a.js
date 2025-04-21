26d9712d722720f56e9876dacc3eb1f9bf2cc658
JotaJotaP <joaojose.cmpinto@gmail.com>
2025-04-21 13:18:32 +0100

    Updates

---
+++ precache-manifest.6e8ab4686a737e4adb410abd069fa92a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7d6491be9f61eed21e61698e3e560b5",
+    "url": "/ecommerce-front/index.html"
+  },
+  {
+    "revision": "959b468bbc2484487a67",
+    "url": "/ecommerce-front/static/css/2.97a18058.chunk.css"
+  },
+  {
+    "revision": "eec5ee4f5d1bc0608d4a",
+    "url": "/ecommerce-front/static/css/main.f5da1b96.chunk.css"
+  },
+  {
+    "revision": "959b468bbc2484487a67",
+    "url": "/ecommerce-front/static/js/2.50be0eff.chunk.js"
+  },
+  {
+    "revision": "eec5ee4f5d1bc0608d4a",
+    "url": "/ecommerce-front/static/js/main.285548eb.chunk.js"
+  },
+  {
+    "revision": "e8ce7a59a0ca6af62aa5",
+    "url": "/ecommerce-front/static/js/runtime~main.d94dac29.js"
+  }
+]);
